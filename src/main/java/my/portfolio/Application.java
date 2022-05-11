0bf6b93f3674fd3785e76ba7bf500f2ad7a1fa36
Ingyu <gyu1039@naver.com>
2022-05-11 11:42:34 +0900

    깃 테스트

--- src/main/java/my/portfolio/Application.java
+++ src/main/java/my/portfolio/Application.java
@@ -1,13 +1,13 @@
-package js.lesson;
+package my.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LessonApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LessonApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-	// git Test 2
+
 }
